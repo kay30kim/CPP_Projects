46694f0b956ab71adad3bbd1459b2532c73995f7
Doss Lee <driversivese@gmail.com>
2025-06-11 16:03:50 -0700

    phone book

--- _Doss/mod00/ex01/phone_book.cc
+++ _Doss/mod00/ex01/phone_book.cc
@@ -1,5 +1,43 @@
 #include <iostream>
+#include <memory>
+#include <string>
+
+struct Contact {
+    std::string firstName;
+    std::string lastName;
+
+    std::string phoneNumber;
+    std::string secret;     // I love Spy Family
+};
+
+class PhoneBook {
+public:
+    void add() {
+        
+    }
+
+    void search() {
+
+    }
+private:
+    const static std::size_t MAX_SIZE = 8;
+    
+    Contact* contacts;
+    std::size_t currentIndex;
+};
 
 int main() {
+    auto pb = std::make_unique<PhoneBook>();
+    auto cmd = std::string();
+
+    while(true) {
+        getline(std::cin, cmd);
+
+        if(cmd.compare("EXIT") == 0) {
+            std::exit(0);
+        }
+    }
+
+    
     return 0;
 }
