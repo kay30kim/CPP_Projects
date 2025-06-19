a07bcd5f7e874632456cb576ed4550bd206d9701
Doss Lee <driversivese@gmail.com>
2025-06-18 23:08:11 -0700

    Kinpai

--- _Doss/mod01/ex04/sed.cc
+++ _Doss/mod01/ex04/sed.cc
@@ -1,7 +1,31 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 
+auto replace_string(
+    std::string& source,
+    const std::string& from,
+    const std::string& to
+) -> std::string {
+    if(from.empty()) return source;
+
+    auto result = std::stringstream();
+    auto i = std::size_t(0);
+
+    while(i < source.length()) {
+        if(source.substr(i, from.length()) == from) {
+            result << to;
+            i += from.length();
+        }
+        else {
+            result << source[i];
+            i++;
+        }
+    }
+
+    return result.str();
+}
 /*
     C-Styled code...
 */
@@ -12,25 +36,30 @@ int main(int argc, char* argv[]) {
     }
 
     auto file_name  = argv[1];
-    //auto origin     = argv[2];
-    //auto replace    = argv[3];
+    auto origin     = argv[2];
+    auto replace    = argv[3];
 
-    auto file_handler = std::ifstream();
-    file_handler.exceptions( std::ifstream::badbit );
-
+    std::ifstream file_handler;
+    
     try {
         file_handler.open(file_name);
+
         auto line = std::string();
+        auto txt = std::stringstream();
 
         while(std::getline(file_handler, line)) {
-            std::cout<<line<<std::endl;
+            auto rep = replace_string(line, origin, replace);
+            txt << rep << '\n';
         }
+
+        std::ofstream output(file_name, std::ios::out | std::ios::trunc);
+
+        output << txt.str();
+        output.close();
     }
     catch(const std::ifstream::failure& e) {
         std::cout<<e.what()<<std::endl;
     }
-
-    file_handler.close();
 
     return 0;
 }
